a39568f45ed0a5880bd4b14876d8ee462f35e6d6
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-02-10 13:48:42 -0800

    Starting up the crime model and making settings applicable to any system and any database setup

--- collateral_consequence/collateral_consequence/settings.py
+++ collateral_consequence/collateral_consequence/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'collateral_consequence.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
+        'NAME': os.environ.get("DB_NAME", os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get("DB_USER", ''),
+        'PASSWORD': os.environ.get('DB_PASS', ''),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'TEST': {
+            'NAME': 'collateral_test',
+        }
     }
 }
 
