11cedac53e44d7735d7e92b1abeb3bccdc5c7a73
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-02-11 10:24:42 -0800

    Alterations to how it'll appear in the django admin

--- collateral_consequence/crimes/admin.py
+++ collateral_consequence/crimes/admin.py
@@ -8,7 +8,15 @@ from crimes.models import Crime
 class CrimeAdmin(admin.ModelAdmin):
     """Administrator for the crime model."""
 
-    list_display = ('name')
+    list_display = (
+        'name', 'crime_type', 'crime_class', 'sex_offense',
+        'county', 'city', 'state'
+    )
+    empty_value_display = '---'
+    list_filter = (
+        'crime_type', 'crime_class', 'sex_offense',
+        'county', 'city', 'state'
+    )
 
     class Meta:
         """Declare which model we're working with."""
