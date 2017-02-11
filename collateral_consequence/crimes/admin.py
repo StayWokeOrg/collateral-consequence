bf19c576ce60f1764376641689c405cd486eed92
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-02-11 10:18:20 -0800

    A few more settings changes, changes to the admin, and an update of requirements.pip

--- collateral_consequence/crimes/admin.py
+++ collateral_consequence/crimes/admin.py
@@ -1,3 +1,18 @@
+"""Administration configuration for the crimes app."""
 from django.contrib import admin
+from crimes.models import Crime
 
 # Register your models here.
+
+
+class CrimeAdmin(admin.ModelAdmin):
+    """Administrator for the crime model."""
+
+    list_display = ('name')
+
+    class Meta:
+        """Declare which model we're working with."""
+
+        model = Crime
+
+admin.site.register(Crime, CrimeAdmin)
