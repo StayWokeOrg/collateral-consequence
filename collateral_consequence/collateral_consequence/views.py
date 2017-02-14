97f17350ed577e596292f28a2af57ab3e24fad5a
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-02-13 20:02:23 -0800

    Updating the requirements.pip and api stubs

--- collateral_consequence/collateral_consequence/views.py
+++ collateral_consequence/collateral_consequence/views.py
@@ -1,11 +1,16 @@
 """Views for the collateral_consequence app."""
 from collateral_consequence import scraper
 from collateral_consequence.forms import StateForm
-from crimes.models import Crime
+from crimes.models import Crime, STATES
 from crimes.processing import process_spreadsheet
+from crimes.serializers import CrimeSerializer
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from urllib.error import HTTPError
 
@@ -36,6 +41,38 @@ def add_state(request):
     return render(
         request, "main/ingest_ready.html", {"form": StateForm()}
     )
+
+
+def crime_search(request):
+    """Retrieve a crime's consequences based on criteria."""
+    return render(request, "", {})
+
+
+@api_view(['GET'])
+def crimes_by_state(request, state=None):
+    """Given a state, retrieve all crime objects for that state."""
+    if request.method == "GET" and state in STATES:
+        crimes = Crime.objects.filter(state=state).all()
+        serialized = CrimeSerializer(crimes, many=True)
+        return Response(serialized.data)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['GET'])
+# def offense_types_by_state(request, state=None):
+#     """Given a state, retrieve narrowed offense types."""
+#     if request.method == 'GET':
+#         crimes = Crime.objects.filter(state=state).all()
+#     return render(request, "", {})
+
+
+def citations_by_state(request):
+    """Given a state, retrieve all the citations for that state."""
+    return render(request, "", {})
+
+
+def crime_titles_by_state(request):
+    """Given a state, retrieve all the crime titles for that state."""
+    return render(request, "", {})
 
 
 def ingest_rows(data, state):
