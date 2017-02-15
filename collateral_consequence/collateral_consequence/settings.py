48b355e2cc3c1217af31d2b72dbed783a8014347
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-02-14 19:53:39 -0800

    Allowed hosts

--- collateral_consequence/collateral_consequence/settings.py
+++ collateral_consequence/collateral_consequence/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", "True"))
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
