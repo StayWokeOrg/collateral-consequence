773fe56d9ec8b876809b62f2d0fb5393fac4c1bd
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-02-14 19:24:45 -0800

    Updating requirements and database settings

--- collateral_consequence/collateral_consequence/settings.py
+++ collateral_consequence/collateral_consequence/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,18 +78,22 @@ WSGI_APPLICATION = 'collateral_consequence.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
-        'NAME': os.environ.get("DB_NAME", os.path.join(BASE_DIR, 'db.sqlite3')),
-        'USER': os.environ.get("DB_USER", ''),
-        'PASSWORD': os.environ.get('DB_PASS', ''),
-        'HOST': os.environ.get('DB_HOST', ''),
-        'TEST': {
-            'NAME': 'collateral_test',
+try:
+    DATABASES = {}
+    DATABASES['default'] = dj_database_url.config(default=os.environ["DATABASE_URL"])
+except KeyError:
+    DATABASES = {
+        'default': {
+            'ENGINE': os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
+            'NAME': os.environ.get("DB_NAME", os.path.join(BASE_DIR, 'db.sqlite3')),
+            'USER': os.environ.get("DB_USER", ''),
+            'PASSWORD': os.environ.get('DB_PASS', ''),
+            'HOST': os.environ.get('DB_HOST', ''),
+            'TEST': {
+                'NAME': 'collateral_test',
+            }
         }
     }
-}
 
 
 # Password validation
